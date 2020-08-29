e7b7d9aa768e1484f234caf43e2cb4ae0b3556ab
infomuscle <infomuscle10@gmail.com>
2020-08-29 16:49:33 +0900

    Basic Background Scroll

---
+++ Assets/Scripts/ObjectScroller.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScroller : MonoBehaviour {
+    public float speed;
+
+    void Update() {
+        if (!GameManager.instance.isGameOver) {
+            transform.Translate(Vector3.left * (speed * Time.deltaTime));
+        }
+    }
+}
