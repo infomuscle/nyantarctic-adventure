90f6d39f0fded7503f42110e0ae49161e132a269
infomuscle <infomuscle10@gmail.com>
2020-08-29 20:26:54 +0900

    Backup for Refactroing Platform Move

--- Assets/Scripts/PlatformMover.cs
+++ Assets/Scripts/PlatformMover.cs
@@ -7,53 +7,74 @@ public class PlatformMover : MonoBehaviour {
     public float speed = 1000f;
     public float width;
 
+    public bool isMove = false;
+
     private void Awake() {
         BoxCollider2D platformCollider = GetComponent<BoxCollider2D>();
         width = platformCollider.size.x;
     }
 
-    private void Start() {
-        if (this.tag == "StandPlatform") {
-            // Debug.Log(transform.position.x);
-            // Debug.Log(width);
-            // Debug.Log(transform.localScale.x);
+    private void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            isMove = true;
+        }
+
+        if (isMove) {
             MovePlatform();
         }
-    }
 
-    private void Update() {
-        // Debug.Log(transform.position.x);
-        // if (!CheckPosition()) {
-            // MovePlatform();
-        // }
+        if (this.tag == "StandPlatform" && CheckOutOfScreen()) {
+            Repositon();
+        }
 
-        // CheckPosition();
+        if (this.tag == "TargetPlatform" && CheckEndOfScreen()) {
+            isMove = false;
+            float targetPositionX = -180 + (width * transform.localScale.x / 2);
+            transform.position = new Vector3(targetPositionX, -224, 0);
+        }
     }
 
     private void MovePlatform() {
-        // Debug.Log("MovePlatform!");
-        switch (this.tag) {
-            case "StandPlatform":
-                // Move to the Left
-                // transform.Translate(Vector3.left * (speed * Time.deltaTime));
-                while (!CheckPosition()) {
-                    transform.Translate(Vector3.left * speed);
-                }
-                break;
-            case "TargetPlatform":
-                // Move and Disappear
-                // transform.Translate(Vector3.left * (speed * Time.deltaTime));
-                break;
-        }
+        transform.Translate(Vector3.left * (speed * Time.deltaTime));
     }
 
-    private bool CheckPosition() {
-        float positionX = -180 - (width * transform.localScale.x / 2);
-        if (transform.position.x <= positionX) {
-            // transform.position = new Vector3(0, -224, 0);
+    private bool CheckOutOfScreen() {
+        float targetPositionX = -180 - (width * transform.localScale.x / 2);
+        if (transform.position.x <= targetPositionX) {
             return true;
         }
 
         return false;
+    }
+
+    private bool CheckEndOfScreen() {
+        float targetPositionX = -180 + (width * transform.localScale.x / 2);
+        if (transform.position.x <= targetPositionX) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void Repositon() {
+        isMove = false;
+        switch (this.tag) {
+            case "StandPlatform":
+                break;
+            case "TargetPlatform":
+                break;
+        }
+        transform.position = new Vector3(0, -224, 0);
+    }
+
+    private void ChangeTag() {
+        switch (this.tag) {
+            case "StandPlatform":
+                this.tag = "TargetPlatform";
+                break;
+            case "TargetPlatform":
+                this.tag = "StandPlatform";
+                break;
+        }
     }
 }
