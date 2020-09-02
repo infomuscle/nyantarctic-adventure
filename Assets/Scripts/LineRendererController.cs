7ee26a725f958e393d09566a62df220c4196475b
infomuscle <infomuscle10@gmail.com>
2020-09-03 00:18:43 +0900

    Line Renderer Skeleton

---
+++ Assets/Scripts/LineRendererController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererController : MonoBehaviour {
+    public LineRenderer lineRenderer;
+
+    void Start() {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.SetColors(Color.white, Color.white);
+        lineRenderer.SetWidth(10f, 10f);
+
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, transform.position + new Vector3(0, 100, 0));
+    }
+
+
+    void Update() { }
+}
