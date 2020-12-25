45ad7ec03e2573b85a44d70bf41f7b73542093a6
infomuscle <infomuscle10@gmail.com>
2020-12-25 21:33:22 +0900

    Tested Admob BannerView

--- Assets/Scripts/AdmobManager.cs
+++ Assets/Scripts/AdmobManager.cs
@@ -3,12 +3,32 @@ using UnityEngine;
 
 public class AdmobManager : MonoBehaviour
 {
+    private BannerView bannerView;
+
     void Start()
     {
         MobileAds.Initialize(initStatus => { });
+
+        this.RequestBanner();
     }
 
-    void Update()
+
+    private void RequestBanner()
     {
+#if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+#elif UNITY_IPHONE
+        string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+#else
+            string adUnitId = "unexpected_platform";
+#endif
+
+        this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+
+        // Create an empty ad request.
+        AdRequest request = new AdRequest.Builder().Build();
+
+        // Load the banner with the request.
+        this.bannerView.LoadAd(request);
     }
 }
