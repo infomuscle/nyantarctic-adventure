b7178e955779e4655d98c08f603a9f2121cb80d1
infomuscle <infomuscle10@gmail.com>
2020-12-25 21:10:30 +0900

    Settings for Admob

---
+++ Assets/Scripts/AdmobManager.cs
@@ -0,0 +1,14 @@
+﻿using GoogleMobileAds.Api;
+using UnityEngine;
+
+public class AdmobManager : MonoBehaviour
+{
+    void Start()
+    {
+        MobileAds.Initialize(initStatus => { });
+    }
+
+    void Update()
+    {
+    }
+}
