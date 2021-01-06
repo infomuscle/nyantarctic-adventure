97f80cdce7312f20ddd70731b02ba077a5601084
infomuscle <infomuscle10@gmail.com>
2021-01-06 20:45:56 +0900

    Added Bool Int Converter

--- Assets/Scripts/SettingsManager.cs
+++ Assets/Scripts/SettingsManager.cs
@@ -9,18 +9,27 @@ public class SettingsManager : MonoBehaviour {
     void Start() {
         bgmSlider.value = PlayerPrefs.GetFloat("bgmVol", 0.7f);
         sfxSlider.value = PlayerPrefs.GetFloat("sfxVol", 0.7f);
-        vibToggle.isOn = PlayerPrefs.GetInt("vibOn", 1) == 1;
+        vibToggle.isOn = IntToBool(PlayerPrefs.GetInt("vibOn", 1));
     }
 
     public void SaveChanges() {
         Debug.Log("SaveChanges");
         PlayerPrefs.SetFloat("bgmVol", bgmSlider.value);
         PlayerPrefs.SetFloat("sfxVol", sfxSlider.value);
-        PlayerPrefs.SetInt("vibOn", vibToggle.isOn ? 1 : 0);
+        PlayerPrefs.SetInt("vibOn", BoolToInt(vibToggle.isOn));
     }
 
     public void SetDefault() {
         PlayerPrefs.SetFloat("bgmVol", 0.7f);
         PlayerPrefs.SetFloat("sfxVol", 0.7f);
+    }
+
+
+    private int BoolToInt(bool value) {
+        return value ? 1 : 0;
+    }
+
+    private bool IntToBool(int value) {
+        return value > 0;
     }
 }
