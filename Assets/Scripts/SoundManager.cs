e8bdc029b730ae060fe31bb3aa1c003570ca05a4
infomuscle <infomuscle10@gmail.com>
2021-01-19 23:06:28 +0900

    Added Sound Manager Skeleton

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+    public static SoundManager instance;
+
+    private AudioSource audioSource;
+
+    private void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+        else {
+            Debug.LogWarning("Multple SoundManagers on Scene");
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(this);
+    }
+
+    public void Start() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(AudioClip clip) {
+        audioSource.clip = clip;
+        audioSource.Play();
+    }
+}
